ce69054763fc08754383f95b21010e797c591fb8
sasanquaa <arestvn@gmail.com>
2023-10-08 20:25:29 +0700

    Add Drop impl and Boxed slice conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,34 @@
-use std::{alloc, fmt, mem, ptr, slice};
+#![feature(allocator_api)]
+
+use alloc::{Global, Layout};
+use ptr::NonNull;
+use std::{alloc, fmt, mem, ptr};
+use std::alloc::Allocator;
 use std::cmp::max;
 use std::fmt::{Formatter, Write};
 use std::ops::Add;
 
-pub struct GapBuffer<T> {
-    buffer: ptr::NonNull<T>,
+pub struct GapBuffer<T, A: Allocator = Global> {
+    allocator: A,
+    buffer: NonNull<T>,
     buffer_capacity: usize,
     gap_start: usize,
     gap_len: usize,
 }
 
-impl<T> GapBuffer<T> {
+impl<T> GapBuffer<T, Global> {
+    pub fn new() -> Self {
+        Self {
+            allocator: Global,
+            buffer: NonNull::dangling(),
+            buffer_capacity: 0,
+            gap_start: 0,
+            gap_len: 0,
+        }
+    }
+}
+
+impl<T, A: Allocator> GapBuffer<T, A> {
     const MIN_NON_ZERO_CAP: usize = if mem::size_of::<T>() == 1 {
         8
     } else if mem::size_of::<T>() <= 1024 {
@@ -19,38 +37,59 @@ impl<T> GapBuffer<T> {
         1
     };
 
-    pub fn new() -> Self {
+    pub fn new_with_allocator(allocator: A) -> Self {
         Self {
-            buffer: ptr::NonNull::dangling(),
+            allocator,
+            buffer: NonNull::dangling(),
             buffer_capacity: 0,
             gap_start: 0,
             gap_len: 0,
         }
     }
 
+    pub fn get(&self, i: usize) -> Option<&T> {
+        if i >= self.len() {
+            None
+        } else {
+            let offset = if i < self.gap_start {
+                i
+            } else {
+                i + self.gap_start + self.gap_len
+            };
+            Some(unsafe { &*self.buffer.as_ptr().add(offset) })
+        }
+    }
+
     pub fn push(&mut self, value: T) {
-        self.insert(self.buffer_size(), value)
+        self.insert(self.len(), value)
     }
 
     pub fn insert(&mut self, i: usize, value: T) {
-        if i > self.buffer_size() {
-            panic!("Index out of bound for {:?} of buffer's size {:?}", i, self.buffer_size())
+        if i > self.len() {
+            panic!("Index out of bound for {:?} of buffer's size {:?}", i, self.len())
         }
         self.gap_move_to(i);
         self.gap_ensure_size(1);
-        unsafe {
-            *self.buffer.as_ptr().add(self.gap_start) = value;
-        }
+        unsafe { *self.buffer.as_ptr().add(self.gap_start) = value; }
         self.gap_start += 1;
         self.gap_len -= 1;
     }
 
-    pub fn delete(&mut self, i: usize) {
-        if self.buffer_size() == 0 || i > self.buffer_size() {
-            panic!("Index out of bound for {:?} of buffer's size {:?}", i, self.buffer_size())
+    pub fn delete(&mut self, i: usize) -> T {
+        if self.len() == 0 || i > self.len() {
+            panic!("Index out of bound for {:?} of buffer's size {:?}", i, self.len())
         }
         self.gap_move_to(i);
         self.gap_len += 1;
+        unsafe { self.buffer.as_ptr().add(self.gap_start).read() }
+    }
+
+    pub fn len(&self) -> usize {
+        self.buffer_capacity - self.gap_len
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.buffer_capacity
     }
 
     fn gap_move_to(&mut self, i: usize) {
@@ -77,49 +116,71 @@ impl<T> GapBuffer<T> {
         if self.gap_len < size {
             let new_capacity = max(self.buffer_capacity * 2, size);
             let new_capacity = max(new_capacity, GapBuffer::<T>::MIN_NON_ZERO_CAP);
-            let new_gap_len = new_capacity - self.buffer_size();
-            let new_layout = alloc::Layout::array::<T>(new_capacity).unwrap();
-            let new_buffer = unsafe { alloc::alloc(new_layout) } as *mut T;
-            let buffer = self.buffer.as_ptr();
-            unsafe {
-                let src = buffer;
-                let dst = new_buffer;
-                let count = self.gap_start;
-                ptr::copy_nonoverlapping::<T>(src, dst, count);
-
-                let src = buffer.add(self.gap_start).add(self.gap_len);
-                let dst = new_buffer.add(self.gap_start).add(new_gap_len);
-                let count = self.buffer_capacity - self.gap_len - self.gap_start;
-                ptr::copy_nonoverlapping::<T>(src, dst, count);
-            }
-            if let Some(layout) = self.buffer_layout() {
-                unsafe { alloc::dealloc(buffer as *mut u8, layout) }
-            }
-            self.buffer = ptr::NonNull::new(new_buffer).unwrap();
+            let new_gap_len = new_capacity - self.len();
+            let new_layout = Layout::array::<T>(new_capacity).unwrap();
+            let new_buffer = if let Some(old_layout) = self.buffer_layout() {
+                unsafe {
+                    self.allocator.grow(self.buffer.cast(), old_layout, new_layout).unwrap().cast()
+                }
+            } else {
+                self.allocator.allocate(new_layout).unwrap().cast()
+            };
+            self.buffer = new_buffer;
             self.buffer_capacity = new_capacity;
             self.gap_len = new_gap_len;
         }
     }
 
-    fn buffer_size(&self) -> usize {
-        self.buffer_capacity - self.gap_len
+    fn buffer_extend_from_vec(&mut self, vec: Vec<T>) {
+        self.gap_ensure_size(vec.len());
+        for value in vec {
+            self.push(value)
+        }
     }
 
-    fn buffer_layout(&self) -> Option<alloc::Layout> {
+    fn buffer_layout(&self) -> Option<Layout> {
         if self.buffer_capacity == 0 {
             None
         } else {
-            Some(alloc::Layout::array::<T>(self.buffer_capacity).unwrap())
+            Some(Layout::array::<T>(self.buffer_capacity).unwrap())
         }
     }
 }
 
-impl<T: Clone> From<GapBuffer<T>> for Box<[T]> {
+impl<T, A: Allocator> Drop for GapBuffer<T, A> {
+    fn drop(&mut self) {
+        unsafe {
+            let ptr = self.buffer.as_ptr();
+            let len = self.gap_start;
+            ptr::drop_in_place(ptr::slice_from_raw_parts_mut(ptr, len));
+
+            let ptr = ptr.add(self.gap_start).add(self.gap_len);
+            let len = self.buffer_capacity - self.gap_start - self.gap_len;
+            ptr::drop_in_place(ptr::slice_from_raw_parts_mut(ptr, len));
+
+            if let Some(layout) = self.buffer_layout() {
+                self.allocator.deallocate(self.buffer.cast(), layout);
+            }
+        }
+    }
+}
+
+impl<T> From<GapBuffer<T>> for Box<[T]> {
     fn from(value: GapBuffer<T>) -> Self {
-        let ptr = value.buffer.as_ptr();
-        let first = unsafe { slice::from_raw_parts(ptr, value.gap_start) };
-        let second = unsafe { slice::from_raw_parts(ptr.add(value.gap_start).add(value.gap_len), value.buffer_capacity - value.gap_len - value.gap_start) };
-        [first, second].concat().into_boxed_slice()
+        let mut value = value;
+        let mut vec = Vec::<T>::with_capacity(value.len());
+        for _ in 0..value.len() {
+            vec.push(value.delete(0));
+        }
+        vec.into_boxed_slice()
+    }
+}
+
+impl<T> From<Box<[T]>> for GapBuffer<T> {
+    fn from(value: Box<[T]>) -> Self {
+        let mut buffer = GapBuffer::<T>::new();
+        buffer.buffer_extend_from_vec(value.into_vec());
+        buffer
     }
 }
 
@@ -152,21 +213,32 @@ mod tests {
     fn gap_buffer_with_capacity_8() {
         let mut buf = GapBuffer::<u8>::new();
         buf.gap_ensure_size(1);
-        assert_eq!(buf.gap_len, GapBuffer::<u8>::MIN_NON_ZERO_CAP)
+        assert_eq!(buf.gap_len, GapBuffer::<u8>::MIN_NON_ZERO_CAP);
     }
 
     #[test]
     fn gap_buffer_with_capacity_4() {
         let mut buf = GapBuffer::<[u8; 1024]>::new();
         buf.gap_ensure_size(1);
-        assert_eq!(buf.gap_len, GapBuffer::<[u8; 1024]>::MIN_NON_ZERO_CAP)
+        assert_eq!(buf.gap_len, GapBuffer::<[u8; 1024]>::MIN_NON_ZERO_CAP);
     }
 
     #[test]
     fn gap_buffer_with_capacity_1() {
         let mut buf = GapBuffer::<[u8; 1025]>::new();
         buf.gap_ensure_size(1);
-        assert_eq!(buf.gap_len, GapBuffer::<[u8; 1025]>::MIN_NON_ZERO_CAP)
+        assert_eq!(buf.gap_len, GapBuffer::<[u8; 1025]>::MIN_NON_ZERO_CAP);
+    }
+
+    #[test]
+    fn gap_buffer_get() {
+        let mut buf = GapBuffer::<u8>::new();
+        buf.push(1);
+        buf.push(2);
+        buf.push(3);
+        assert_eq!(Some(&1u8), buf.get(0));
+        assert_eq!(Some(&2u8), buf.get(1));
+        assert_eq!(Some(&3u8), buf.get(2));
     }
 
     #[test]
@@ -176,7 +248,7 @@ mod tests {
         buf.push(2);
         buf.push(3);
         let boxed = Box::<[u8]>::from(buf);
-        assert_eq!(&[1, 2, 3], boxed.as_ref())
+        assert_eq!(&[1, 2, 3], boxed.as_ref());
     }
 
     #[test]
@@ -188,7 +260,7 @@ mod tests {
         buf.insert(0, 5);
         buf.insert(3, 6);
         let boxed = Box::<[u8]>::from(buf);
-        assert_eq!(&[5, 1, 2, 6, 3], boxed.as_ref())
+        assert_eq!(&[5, 1, 2, 6, 3], boxed.as_ref());
     }
 
 
@@ -201,7 +273,7 @@ mod tests {
         buf.delete(0);
         buf.delete(1);
         let boxed = Box::<[u8]>::from(buf);
-        assert_eq!(&[2], boxed.as_ref())
+        assert_eq!(&[2], boxed.as_ref());
     }
 
     #[test]
